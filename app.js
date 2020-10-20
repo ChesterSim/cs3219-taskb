54a8dd54570f901fb8caa7693519d079b36076eb
Chester Sim <chestersimdy@gmail.com>
2020-10-20 14:44:16 +0800

    Add heroku

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -5,10 +7,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 const todosRouter = require("./controllers/todos");
+console.log(process.env.ENVIRONMENT);
+let mongoDbUri;
+if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+  mongoDbUri = "mongodb://127.0.0.1:27017/todo-app"; // 27017 is the default port for MongoDB
+} else {
+  mongoDbUri = "mongodb+srv://package-dev:99vnEMFTdkOmn0Is@todo-app.01jd0.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+}
 
-mongoose.connect("mongodb://127.0.0.1:27017/todo-app", // 27017 is the default port for MongoDB
+mongoose.connect(mongoDbUri,
   { useNewUrlParser: true, useFindAndModify: false })
-  // .then(() => console.log("Connected to local MongoDB")) // commented out for testing
+  .then(() => console.log(`Connected to ${process.env.ENVIRONMENT} MongoDB`))
   .catch(err => console.log("Error connecting to MongoDB:", err.message));
 
 app.use(cors());
