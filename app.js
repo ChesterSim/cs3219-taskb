55af1654f0fb12a9c1bad357540dfa96fe8d6783
Chester Sim <chestersimdy@gmail.com>
2020-10-20 20:18:14 +0800

    Add Procfile

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const mongoose = require("mongoose");
 
 const app = express();
 const todosRouter = require("./controllers/todos");
-console.log(process.env.ENVIRONMENT);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database setup
 let mongoDbUri;
 if (process.env.ENVIRONMENT === "DEVELOPMENT") {
   mongoDbUri = "mongodb://127.0.0.1:27017/todo-app"; // 27017 is the default port for MongoDB
 } else {
-  mongoDbUri = "mongodb+srv://package-dev:99vnEMFTdkOmn0Is@todo-app.01jd0.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+  mongoDbUri = "mongodb+srv://package-dev:99vnEMFTdkOmn0Is@todo-app.01jd0.mongodb.net/test?retryWrites=true&w=majority"
 }
 
 mongoose.connect(mongoDbUri,
@@ -20,8 +24,6 @@ mongoose.connect(mongoDbUri,
   .then(() => console.log(`Connected to ${process.env.ENVIRONMENT} MongoDB`))
   .catch(err => console.log("Error connecting to MongoDB:", err.message));
 
-app.use(cors());
-app.use(bodyParser.json());
 
 app.use('/api/todos', todosRouter);
 
