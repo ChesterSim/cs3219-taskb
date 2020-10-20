2a1fa2a33bd42a009e62ed25b67f8a879e722d65
Chester Sim <chestersimdy@gmail.com>
2020-10-21 01:59:03 +0800

    Update travis.yml

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const mongoDbUri = process.env.ENVIRONMENT === "DEVELOPMENT"
   ? "mongodb://127.0.0.1:27017/todo-app" // 27017 is the default port for MongoDB
   : process.env.MONGOD_URI;
 mongoose.connect(mongoDbUri,
-  { useNewUrlParser: true, useFindAndModify: false })
+  { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
   .then(() => console.log(`Connected to ${process.env.ENVIRONMENT} MongoDB`))
   .catch(err => console.log("Error connecting to MongoDB:", err.message));
 
